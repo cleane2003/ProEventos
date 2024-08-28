e62b00aafc5f2536f254edc98daae511796bc99f
Cleane Evelin <cleaneevelin04@gmail.com>
2024-08-28 15:15:16 -0300

    feat: realizando crud

---
+++ src/ProEventos.Application/Contratos/IEventosServices.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Application.Contratos
+{
+    public interface IEventoService
+    {
+        // EVENTOS
+        Task<Evento> AddEvento(Evento model);
+        Task<Evento> UpdateEvento(int eventoId, Evento model);
+        Task<bool> DeleteEvento(int eventoId);
+
+    
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+        Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes);
+    }
+}
